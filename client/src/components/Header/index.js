30d0d58baf1c7e592af9d9727183cde35c646f01
Craig <panamaworldwideinvestments@gmail.com>
2022-08-23 19:05:41 +0100

    updated searchbar icon

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,7 +13,7 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Hellow World</h1>
+            <h1 className="m-0">Hello World</h1>
           </Link>
         </div>
         <div>
